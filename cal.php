e688913c06f15737303872494adeb3428f0fcf4a
ehirokatsu <48009332+ehirokatsu@users.noreply.github.com>
2020-02-27 12:45:00 +0900

    Add files via upload
    20200227_1244

--- cal.php
+++ cal.php
@@ -22,14 +22,14 @@
     // 月は1～12までの数値
     if (
         isset($_GET["year"]) &&
-        isset($_GET["month"])&&
-        preg_match('/^[0-9]{0,4}$/', $_GET["year"]) && 
-        $_GET["year"] > 0 && 
+        isset($_GET["month"]) &&
+        preg_match('/^[0-9]{0,4}$/', $_GET["year"]) &&
+        $_GET["year"] > 0 &&
         preg_match('/^[1-9]$|^0[1-9]$|^1[0-2]$/', $_GET["month"])
         ) {
             $year = (int)$_GET["year"];
             $month = (int)$_GET["month"];
-    // 上記以外の場合は今月のカレンダーを表示する
+    // 上記以外の場合は今月のカレンダーを表示
     } else {
         $year = (int)$date->format('Y');
         $month = (int)$date->format('n');
@@ -38,12 +38,12 @@
     // 表示するカレンダーの年月を表示
     echo '<div class="center">'.$year.'年'.$month.'月</div><br>';
 
+    // 表示するカレンダーの1日を取得
     $firstDate = new DateTime("{$year}-{$month}-1");
     // 月初めの曜日
     $dayOfWeek = $firstDate->format('w');
     // 月の日数
     $dayMax = $firstDate->format('t');
-
 
     echo '<table border="1" cellpadding="5" cellspacing="0" align="center">';
         // カレンダーを表示
@@ -57,9 +57,9 @@
             echo '<td align="center" bgcolor="#eeeeee">土</td>';
         echo '</tr>';
 
-		// 日にち用カウンタ
-    	$day = 1;
-    	
+        // 日にち用カウンタ
+        $day = 1;
+        
         echo '<tr>';
         // カレンダー1行目の出力
         for ($j = 0; $j <= 6; $j++) {
@@ -74,7 +74,7 @@
         }
         echo '</tr>';
         // カレンダー2行目以降の出力
-        for (; $day+7 <= $dayMax+1; ) {
+        while ($day + 7 <= $dayMax + 1) {
             echo '<tr>';
             for ($j = 0; $j <= 6; $j++){
                 echo '<td align="center">'.$day.'</td>';
@@ -83,7 +83,7 @@
             echo '</tr>';
         }
         // 最終日以降パディングが必要なら出力
-        if ($day < $dayMax+1) {
+        if ($day < $dayMax + 1) {
             echo '<tr>';
             for ($j = 0; $j <= 6; $j++) {
                 // 月末まで
@@ -101,18 +101,18 @@
 
 
     // 前月ボタン押下時の準備
-    $monthBefore = $month -1;
+    $monthBefore = $month - 1;
     $yearBefore = $year;
-    // 前月が0月になると、年を1年戻して12月にする
+    // 前月が0月になると、年を1年戻して12月に変換
     if ($monthBefore == 0) {
-        $monthBefore =12;
+        $monthBefore = 12;
         $yearBefore = $year - 1;
     }
 
     // 来月ボタン押下時の準備
-    $monthAfter = $month +1;
+    $monthAfter = $month + 1;
     $yearAfter = $year;
-    // 来月が13月になると、年を1年上げて1月にする
+    // 来月が13月になると、年を1年上げて1月に変換
     if ($monthAfter == 13) {
         $monthAfter = 1;
         $yearAfter = $year + 1;
@@ -122,7 +122,7 @@
 <br>
 
 <div class="center">
-    <form name="now" method="GET" action="cal2.php">
+    <form name="now" method="GET" action="cal.php">
     <input type="submit" value="今月">
     </form>
 </div>
@@ -132,7 +132,7 @@
 <table align="center">
     <tr>
         <td>
-            <form name="before" method="GET" action="cal2.php">
+            <form name="before" method="GET" action="cal.php">
             <input type="hidden" name="year" value="<?php echo $yearBefore; ?>">
             <input type="hidden" name="month" value="<?php echo $monthBefore; ?>">
             <input type="submit" value="前月">
@@ -142,7 +142,7 @@
         <td></td>
         
         <td>
-            <form name="after" method="GET" action="cal2.php">
+            <form name="after" method="GET" action="cal.php">
             <input type="hidden" name="year" value="<?php echo $yearAfter; ?>">
             <input type="hidden" name="month" value="<?php echo $monthAfter; ?>">
             <input type="submit" value="来月">
@@ -150,7 +150,6 @@
         </td>
     </tr>
 </table>
-
 
 </p>
 </body>
